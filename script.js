272a9c0dc9391192541fce5badb1cdc0c41f6312
david12334556 <998901233289david@gmail.com>
2026-07-02 17:19:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -217,3 +217,6 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+// Автоматическая установка иконки тортика на вкладку браузера
+document.head.insertAdjacentHTML('beforeend', '<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎂</text></svg>">');
